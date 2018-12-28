93f08e6bc9ac140dab93f8136c3596d1a60a8cb3
BoGustafssonBNI <bo.gustafsson@su.se>
2018-12-28 16:51:19 +0100

    Initial Commit

---
+++ gribParserTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gribParserTest
+//
+//  Created by Bo Gustafsson on 2018-12-28.
+//  Copyright © 2018 Bo Gustafsson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gribParserTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gribParserTest
+//
+//  Created by Bo Gustafsson on 2018-12-28.
+//  Copyright © 2018 Bo Gustafsson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
